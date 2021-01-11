56936c71d9d016b11d3bd18e93894b55d6be0536
vlp <thomas@criscione.fr>
2021-01-11 14:54:16 +0100

    ok erreurs

--- src/main.rs
+++ src/main.rs
@@ -39,120 +39,104 @@ impl Message{
             let trigger_to_add =
                 match get_left_arg(&choice) {
                     Ok(trigger_to_add_ctrl) => trigger_to_add_ctrl,
-                    Err(_e) => return Err("ERROR".to_string()),
+                    Err(e) => return Err(format!("ERROR: trigger_to_add - {}", e)),
                 };
             let answer_to_add =
                 match get_right_arg(&choice) {
                     Ok(answer_to_add_ctrl) => answer_to_add_ctrl,
-                    Err(_e) => return Err("ERROR".to_string()),
+                    Err(e) => return Err(format!("ERROR: answer_to_add - {}", e)),
                 };
             if trigger_to_add == answer_to_add {
-                return Err("ERROR".to_string())
+                return Err("ERROR: 2 arg needed [trigger] [answer]".to_string());
             }
-            let _chat_to_add =
+            let chat_to_add =
                 match add_chat(trigger_to_add, answer_to_add, connection_db, trigger_word_list) {
-                    Ok(_chat_to_add_ctrl) => {
-                        return Ok(format!("[admin mode by: {}] {} ajouté !", &self.sender_name, _chat_to_add_ctrl))
-                    }
-                    Err(_e) => return Err("ERROR".to_string()),
+                    Ok(chat_to_add_ctrl) => Ok(format!("[admin mode by: {}] {} ajouté !", &self.sender_name, chat_to_add_ctrl)),
+                    Err(e) => Err(format!("ERROR: chat_to_add - {}", e)),
             };
+            chat_to_add
         // _mode admin pour suppression de trigger
         } else if botbot_phrase.contains("admin del") && &self.sender_id == "@vlp:matrix.fdn.fr"{
             let trigger_to_del =
                 match get_left_arg(&choice) {
                     Ok(trigger_to_del_ctrl) => trigger_to_del_ctrl,
-                    Err(_e) => return Err("ERROR".to_string()),
+                    Err(e) => return Err(format!("ERROR: trigger_to_del - {}", e)),
                 };
-            let _chat_to_del =
+            let chat_to_del =
                 match del_chat(trigger_to_del, connection_db, trigger_word_list) {
-                    Ok(_chat_to_del_ctrl) => {
-                        return Ok(format!("[admin mode by: {}] {} supprimé !", &self.sender_name, _chat_to_del_ctrl))
-                    }
-                    Err(_e) => return Err("ERROR".to_string()),
+                    Ok(_chat_to_del_ctrl) => Ok(format!("[admin mode by: {}] {} supprimé !", &self.sender_name, _chat_to_del_ctrl)),
+                    Err(e) => Err(format!("ERROR: chat_to_del - {}", e)),
                 };
+            chat_to_del
         // _réponse de botbot
         } else{
-            let _answer =
+            let answer =
                 match return_answer(botbot_phrase, connection_db, trigger_word_list){
                     Ok(answer_ctrl) => {
+                        // _remplace les %s par le nom du sender
                         let answer_with_name= &answer_ctrl[..].replace("%s", &self.sender_name);
                         // _remplace les %n par un retour à la ligne
                         let answer_with_new_line = &answer_with_name[..].replace("%n", "\n");
-                        return Ok(answer_with_new_line.to_string());
+                        Ok(answer_with_new_line.to_string())
                     }
-                    Err(_e) => return Err("ERROR".to_string()),
+                    Err(e) => return Err(format!("ERROR: return answer - {}", e)),
                 };
-            // _remplace les %s par le nom du sender
+            answer
         }
     }
-    fn talking(&self, phrase_to_say: String){
+    fn talking(&self, phrase_to_say: String) -> Result<Child, String> {
         let mut blabla = "-m".to_string();
         blabla.push_str(&phrase_to_say[..]);
         let mut room = "-r".to_string();
         room.push_str(&self.room_id);
-        let _talking_status =
+        let talking_status =
             match Command::new("./../matrix-commander/matrix-commander.py")
             .arg("-c./../matrix-commander/credentials.json")
             .arg("-s./..//matrix-commander/store/")
             .arg(room)
             .arg(blabla)
             .spawn() {
-                Ok(talking_status_ctrl) => talking_status_ctrl,
-                Err(e) => {
-                    println!("ERROR: sending message: {}", e);
-                    return
-                }
-
+                Ok(talking_status_ctrl) => Ok(talking_status_ctrl),
+                Err(e) => Err(format!("ERROR: sending message - {}", e)),
             };
+        talking_status
     }
 }
 
 fn add_chat(trigger: String, answer: String, connection_db: &Connection, trigger_word_list: &mut Vec<String>) -> Result<String, String> {
     let mut insert_statement =
         match connection_db.prepare("INSERT INTO talking (trigger, answer) VALUES (?, ?);"){
-            Ok(insert_statement_ctrl) => {
-                insert_statement_ctrl
-            }
-            Err(e) => {
-                println!("Error add word: {}", e);
-                return Err("ERROR".to_string());
-                }
+            Ok(insert_statement_ctrl) => insert_statement_ctrl,
+            Err(e) => return Err(format!("ERROR: add prepare db - {}", e)),
           };
         insert_statement.bind(1, &trigger[..]).unwrap();
         insert_statement.bind(2, &answer[..]).unwrap();
         let _run_statement =
             match insert_statement.next() {
                 Ok(_run_statement_ctrl) => _run_statement_ctrl,
-                Err(_e) => return Err("ERROR".to_string()),
+                Err(e) => return Err(format!("ERROR: process add trigger - {}", e)),
             };
         trigger_word_list.push(trigger.to_string());
-        println!(" > admin: add '{}'", trigger);
-        return Ok(trigger);
+        Ok(trigger)
 }
 
 fn del_chat(trigger: String, connection_db: &Connection, trigger_word_list: &mut Vec<String>) -> Result<String, String> {
     if !trigger_word_list.contains(&trigger) {
-        return Err("ERROR".to_string())
+        return Err(format!("ERROR: trigger not in db"))
     }
     let mut del_statement =
         match connection_db.prepare("DELETE FROM talking WHERE trigger=?"){
-            Ok(del_statement_ctrl) => {
-                del_statement_ctrl
-            }
-            Err(e) => {
-                println!("Error del word: {}", e);
-                return Err("ERROR".to_string())
-                }
+            Ok(del_statement_ctrl) => del_statement_ctrl,
+            Err(e) => return Err(format!("ERROR: del prepare db - {}", e)),
           };
     del_statement.bind(1, &trigger[..]).unwrap();
     let _run_statement =
         match del_statement.next() {
             Ok(_run_statement_ctrl) => _run_statement_ctrl,
-            Err(_e) => return Err("ERROR".to_string())
+            Err(e) => return Err(format!("ERROR: process del trigger - {}", e)),
         };
     trigger_word_list.retain(|x| *x != trigger);
-    println!(" > admin: del '{}'", trigger);
-    return Ok(trigger);
+    Ok(trigger)
 }
 
 fn return_answer(choice: String, connection_db: &Connection, trigger_word_list: &mut Vec<String>) -> Result<String, String> {
@@ -160,13 +144,8 @@ fn return_answer(choice: String, connection_db: &Connection, trigger_word_list: 
         if choice.contains(&x[..]) {
             let mut select_statement =
                 match connection_db.prepare("SELECT answer FROM talking where trigger=?"){
-                    Ok(select_statement_ctrl) => {
-                        select_statement_ctrl
-                    }
-                    Err(e) => {
-                        println!("Error select word: {}", e);
-                        return Err("ERROR".to_string());
-                        }
+                    Ok(select_statement_ctrl) => select_statement_ctrl,
+                    Err(e) =>  return Err(format!("ERROR: select prepare db - {}", e)),
                   };
             select_statement.bind(1, &x[..]).unwrap();
             while let State::Row = select_statement.next().unwrap() {
@@ -176,141 +155,116 @@ fn return_answer(choice: String, connection_db: &Connection, trigger_word_list: 
             continue;
         }
     }
-    return Err("ERROR".to_string());
+    Err(format!("ERROR: no word found"))
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////  FONCTIONS pour nettoyer les trames de matrix-commander
 
 fn clean_room_origin(raw_room_origin:String) -> Result<String, String> {
     let debut = match raw_room_origin.find("room") {
-        Some(debut_index) => {
-             debut_index + 5
-         }
-         None => {
-             0
-         }
+        Some(debut_index) => debut_index + 5,
+        None => return Err("ERROR: clean_room_origin start".to_string()),
     };
     let fin = match raw_room_origin.find("[") {
-        Some(fin_index) => {
-             fin_index - 1
-         }
-         None => {
-             0
-         }
+        Some(fin_index) => fin_index - 1,
+        None => return Err("ERROR: clean_room_origin end".to_string()),
     };
-    if debut == 0 || fin == 0 || debut > fin {
-        return Err("clean_room ERROR: Matrix-Commander output unreadable".to_string());
+    if debut >= fin {
+        Err("ERROR: clean_room_origin matrix-commander output unreadable".to_string())
+    }else {
+        let clean_room_origin = &raw_room_origin[debut..fin];
+        Ok(clean_room_origin.to_string())
     }
-    let clean_room_origin = &raw_room_origin[debut..fin];
-    return Ok(clean_room_origin.to_string())
 }
 
 fn clean_room_id(raw_room_id:String) -> Result<String, String> {
     let debut = match raw_room_id.find("[") {
-        Some(debut_index) => {
-             debut_index + 1
-         }
-         None => {
-             0
-         }
+        Some(debut_index) => debut_index + 1,
+        None => return Err("ERROR: clean_room_id start".to_string()),
     };
     let fin = match raw_room_id.find("]") {
-        Some(fin_index) => {
-             fin_index
-         }
-         None => {
-             0
-         }
+        Some(fin_index) => fin_index,
+        None => return Err("ERROR: clean_room_id end".to_string()),
     };
-    if debut == 0 || fin == 0 || debut > fin {
-        return Err("clean_room_id ERROR: Matrix-Commander output unreadable".to_string());
+    if debut >= fin {
+        Err("ERROR: clean_room_id matrix-commander output unreadable".to_string())
+    } else {
+        let clean_room_id = &raw_room_id[debut..fin];
+        Ok(clean_room_id.to_string())
     }
-    let clean_room_id = &raw_room_id[debut..fin];
-    return Ok(clean_room_id.to_string())
 }
 
 
 fn clean_sender_id(raw_sender_id:String) -> Result<String, String> {
     let debut = match raw_sender_id.find("[") {
-        Some(debut_index) => {
-             debut_index + 1
-         }
-         None => {
-             0
-         }
+        Some(debut_index) => debut_index + 1,
+        None => return Err("ERROR: clean_sender_id start".to_string()),
     };
     let fin = match raw_sender_id.find("]") {
-        Some(fin_index) => {
-             fin_index
-         }
-         None => {
-             0
-         }
+        Some(fin_index) => fin_index,
+        None => return Err("ERROR: clean_sender_id end".to_string()),
     };
-    if debut == 0 || fin == 0 || debut > fin {
-        return Err("clean_sender_id ERROR: Matrix-Commander output unreadable".to_string());
+    if debut > fin {
+        Err("ERROR: clean_sender_id matrix-commander output unreadable".to_string())
+    } else {
+        let clean_sender_id = &raw_sender_id[debut..fin];
+        Ok(clean_sender_id.to_string())
     }
-    let clean_sender_id = &raw_sender_id[debut..fin];
-    return Ok(clean_sender_id.to_string())
 }
-
 
 fn clean_sender_name(raw_sender_name:String) -> Result<String, String> {
     let debut = match raw_sender_name.find("sender") {
-        Some(debut_index) => {
-             debut_index + 7
-         }
-         None => {
-             0
-         }
+        Some(debut_index) => debut_index + 7,
+        None => return Err("ERROR: clean_sender_name start".to_string()),
     };
     let fin = match raw_sender_name.find("[") {
-        Some(fin_index) => {
-             fin_index - 1
-         }
-         None => {
-             0
-         }
+        Some(fin_index) => fin_index - 1,
+        None => return Err("ERROR: clean_sender_name end".to_string()),
     };
-    if debut == 0 || fin == 0 || debut > fin {
-        return Err("clean_sender_name ERROR: Matrix-Commander output unreadable".to_string());
+    if debut > fin {
+        Err("clean_sender_name ERROR: Matrix-Commander output unreadable".to_string())
+    } else {
+        let raw_sender_name = &raw_sender_name[debut..fin];
+        Ok(raw_sender_name.to_string())
     }
-    let raw_sender_name = &raw_sender_name[debut..fin];
-    return Ok(raw_sender_name.to_string())
 }
 
 fn get_left_arg(admin_msg: &String) -> Result<String, String> {
     let debut_mark =
         match admin_msg.find("[") {
             Some(debut_mark_index) => debut_mark_index + 1,
-            None => 0
+            None => return Err("ERROR: unable to find left arg start".to_string()),
         };
     let fin_mark =
         match admin_msg.find("]") {
             Some(fin_mark_index) => fin_mark_index,
-            None => 0
+            None => return Err("ERROR: unable to find left arg end".to_string()),
         };
-    if debut_mark == 0 || fin_mark == 0 {
-        return Err("get_left_arg ERROR".to_string());
+    if debut_mark == fin_mark {
+        Err("ERROR: no value in left arg".to_string())
     }
-    return Ok(admin_msg[debut_mark..fin_mark].to_string());
+    else {
+        Ok(admin_msg[debut_mark..fin_mark].to_string())
+    }
 }
 
 fn get_right_arg(admin_msg: &String) -> Result<String, String> {
     let debut_mark =
         match admin_msg.rfind("[") {
             Some(debut_mark_index) => debut_mark_index + 1,
-            None => 0
+            None => return Err("ERROR: unable to find right arg start".to_string()),
         };
     let fin_mark =
         match admin_msg.rfind("]") {
             Some(fin_mark_index) => fin_mark_index,
-            None => 0
+            None => return Err("ERROR: unable to find right arg end".to_string()),
         };
-    if debut_mark == 0 || fin_mark == 0 {
-        return Err("get_left_arg ERROR".to_string());
+    if debut_mark == fin_mark {
+        Err("ERROR: no value in right arg".to_string())
     }
-    return Ok(admin_msg[debut_mark..fin_mark].to_string());
+    else {
+        Ok(admin_msg[debut_mark..fin_mark].to_string())
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -361,7 +315,7 @@ fn matrix_commander_daemon_launch() -> Result<Child, Error> {
         .arg("-lforever")
         .stdout(Stdio::piped())
         .spawn();
-    return daemon;
+    daemon
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -504,10 +458,18 @@ fn main() {
                 let _answer =
                     match incoming_message.thinking(&mut trigger_word_list, &connection_db){
                         Ok(answer_ctrl) => {
-                            incoming_message.talking(answer_ctrl);
+                            let _talking_status =
+                                match incoming_message.talking(answer_ctrl){
+                                    Ok(child) => child.id(),
+                                    Err(e) => {
+                                        println!("ERROR talking: {}", e);
+                                        0
+                                    },
+                                };
                         }
-                        Err(_e) => {
-                            continue;
+                        Err(e) => {
+                            println!("ERROR: {}", e);
+                            continue
                         }
                     };
             }
@@ -515,5 +477,4 @@ fn main() {
         // _vide la zone de lecture du buffer à chaque boucle
         line_from_buffer.clear();
     }
-
 }
