0573e56c0fb6f07a7ddda3cbcdb407bebf1bafee
vlp <thomas@criscione.fr>
2021-01-21 12:39:37 +0100

    core sys

--- src/main.rs
+++ src/main.rs
@@ -27,14 +27,14 @@ struct Message{
 
 impl Message{
     // _fonction qui détermine les actions de botbot lorsqu'il est déclenché
-    fn thinking(&self, admin_list: &Vec<String>, trigger_word_list: &mut Vec<String>, connection_db: &Connection) -> Result<String, String> {
+    fn thinking(&self, adminsys_list: &Vec<String>, admincore_list: &Vec<String>, trigger_word_list: &mut Vec<String>, connection_db: &Connection) -> Result<String, String> {
         let choice = String::from(unidecode(&self.m_message).to_string());
         let mut botbot_phrase = String::from(unidecode(&self.m_message).to_string());
         // _uppercases
         botbot_phrase.make_ascii_lowercase();
         // _mode admin
         let answer =
-            if botbot_phrase.contains("botbot admin") && admin_list.contains(&self.sender_id) {
+            if botbot_phrase.contains("botbot admin") && adminsys_list.contains(&self.sender_id) {
                 let admin_answer =
                     // _mode admin pour ajout de trigger
                     if botbot_phrase.contains("admin add") {
@@ -81,7 +81,21 @@ impl Message{
                     };
                 admin_answer
             } else if botbot_phrase.contains("ping adminsys") {
-                let mut iterator = admin_list.iter();
+                let mut iterator = adminsys_list.iter();
+                let mut liste_to_ping = String::from("ping: ");
+                while let Some(x) = iterator.next() {
+                    let fin_mark =
+                        match x.find(":") {
+                            Some(fin_mark_index) => fin_mark_index,
+                            None => continue,
+                        };
+                    liste_to_ping += &x[..fin_mark];
+                    liste_to_ping += ", ";
+                }
+                let chat_to_ping = format!("Hello les adminsys: {} vous contacte ! {}", &self.sender_name, &liste_to_ping[0..liste_to_ping.len()-2]);
+                Ok(chat_to_ping)
+            } else if botbot_phrase.contains("ping admincore") {
+                let mut iterator = admincore_list.iter();
                 let mut liste_to_ping = String::from("ping: ");
                 while let Some(x) = iterator.next() {
                     let fin_mark =
@@ -143,15 +157,34 @@ fn main() {
     let mut trigger_word_list: Vec<String> = Vec::new();
 
     // _liste des admins ayant accès au mode admin de botbot
-    let mut admin_list: Vec<String> = Vec::new();
+    let mut adminsys_list: Vec<String> = Vec::new();
+    let mut admincore_list: Vec<String> = Vec::new();
+
+    // _team ADMINCORE
+    admincore_list.push("@belette:uc.neviani.fr".to_string());
+    admincore_list.push("@afriqs:matrix.fdn.fr".to_string());
+    admincore_list.push("@asmadeus:codewreck.org".to_string());
+    admincore_list.push("@tom28:matrix.fdn.fr".to_string());
+    admincore_list.push("@olb:matrix.org".to_string());
+    admincore_list.push("@vg:matrix.fdn.fr".to_string());
 
     // _team ADMINSYS
-    admin_list.push("@vlp:matrix.fdn.fr".to_string());
-    admin_list.push("@belette:uc.neviani.fr".to_string());
-    admin_list.push("@afriqs:matrix.fdn.fr".to_string());
-    admin_list.push("@asmadeus:codewreck.org".to_string());
-    admin_list.push("@tom28:matrix.fdn.fr".to_string());
-    admin_list.push("@khrys:matrix.fdn.fr".to_string());
+    adminsys_list.push("@vlp:matrix.fdn.fr".to_string());
+    adminsys_list.push("@belette:uc.neviani.fr".to_string());
+    adminsys_list.push("@afriqs:matrix.fdn.fr".to_string());
+    adminsys_list.push("@asmadeus:codewreck.org".to_string());
+    adminsys_list.push("@tom28:matrix.fdn.fr".to_string());
+    adminsys_list.push("@khrys:matrix.fdn.fr".to_string());
+    adminsys_list.push("@olb:matrix.org".to_string());
+    adminsys_list.push("@vg:matrix.fdn.fr".to_string());
+    adminsys_list.push("@blackmoor:matrix.fdn.fr".to_string());
+    adminsys_list.push("@capslock:matrix.fdn.fr".to_string());
+    adminsys_list.push("@dino:matrix.fdn.fr".to_string());
+    adminsys_list.push("@domi:matrix.fdn.fr".to_string());
+    adminsys_list.push("@hamo:matrix.fdn.fr".to_string());
+    adminsys_list.push("@stephaneascoet:matrix.fdn.fr".to_string());
+    adminsys_list.push("@symeon:matrix.fdn.fr".to_string());
+    adminsys_list.push("@youpi:matrix.fdn.fr".to_string());
 
     println!("[Database]");
 
@@ -295,7 +328,7 @@ fn main() {
                     };
                 let clean_message = String::from(raw_data[3]);
                 let incoming_message = Message{_room_origin: clean_room, room_id: clean_room_id, sender_id: clean_sender_id, sender_name: clean_sender_name, m_message: clean_message};
-                match incoming_message.thinking(&admin_list, &mut trigger_word_list, &connection_db){
+                match incoming_message.thinking(&adminsys_list, &admincore_list, &mut trigger_word_list, &connection_db){
                     Ok(answer_ctrl) => {
                         println!("botbot: {}", answer_ctrl);
                         let _talking_status =
